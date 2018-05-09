3cb47f5bc6d11bd3daf92fa769eb3d9ac6bbe385
KadrusBAG <32191410+KadrusBAG@users.noreply.github.com>
2018-05-09 17:46:09 +0300

    Update rb_tree.cpp

--- tests/rb_tree.cpp
+++ tests/rb_tree.cpp
@@ -179,3 +179,197 @@ TEST_CASE("new")
     	tree.print(ostream, tree.root(), 1);
     	REQUIRE(ostream.str() == result);
 }
+
+
+
+
+
+
+TEST_CASE("balance_1")
+{
+    tree_t<int> tree {3 ,2 , 1 };
+	std::string out {
+		"----3\n"
+		"--2\n"
+		"----1\n"
+	};
+
+	std::ostringstream ostream;
+	tree.print(ostream , tree.root(), 1);
+	
+	REQUIRE(ostream.str() == out);
+}
+
+TEST_CASE("balance_2")
+{
+    tree_t<int> tree{5 , 3 , 6 , 8 , 9 , 10 , 12 , 13};
+	std::string out {
+		"--------13\n"
+		"------12\n"
+		"----10\n"
+		"------9\n"
+		"--8\n"
+		"------6\n"
+		"----5\n"
+		"------3\n"
+	};
+
+	std::ostringstream ostream;
+	tree.print(ostream , tree.root(), 1);
+	
+	REQUIRE(ostream.str() == out);
+}
+
+TEST_CASE("balance_3")
+{
+    tree_t<int> tree {1, 2 , 3 , 4 , 5 };
+
+	std::string out {
+		"------5\n"
+		"----4\n"
+		"------3\n"
+		"--2\n"
+		"----1\n"
+	};
+
+	std::ostringstream ostream;
+	tree.print(ostream , tree.root(), 1);
+	
+	REQUIRE(ostream.str() == out);
+}
+
+TEST_CASE("balance_4")
+{
+    tree_t<int> tree {10 , 85 , 15 , 70 , 20 , 60 , 30 , 50 ,65 ,80 ,90 ,40 , 5 ,55 };
+	
+	std::string out {
+		
+		"--------90\n"
+		"------85\n"
+		"--------80\n"
+		"----70\n"
+		"--------65\n"
+		"------60\n"
+		"----------55\n"
+		"--------50\n"
+		"----------40\n"
+		"--30\n"
+		"------20\n"
+		"----15\n"
+		"------10\n"
+		"--------5\n"
+	};
+
+	std::ostringstream ostream;
+	tree.print(ostream , tree.root(), 1);
+	
+	REQUIRE(ostream.str() == out);
+}
+
+TEST_CASE("delition_1")
+{
+    tree_t<int> tree {1 , 2 , 3 ,4 ,5};
+	
+	tree.remove(4);
+	
+	std::string out {
+		
+		"----5\n"
+		"------3\n"
+		"--2\n"
+		"----1\n"
+	};
+	
+	std::ostringstream ostream;
+	tree.print(ostream , tree.root(), 1);
+	
+	REQUIRE(ostream.str() == out);
+}
+
+TEST_CASE("delition_2")
+{
+    tree_t<int> tree {7,3,8,9,1,0};
+	
+	tree.remove(1);
+	
+	std::string out {
+		"------9\n"
+		"----8\n"
+		"--7\n"
+		"----3\n"
+		"------0\n"
+	};
+	std::ostringstream ostream;
+	tree.print(ostream , tree.root(), 1);
+	
+	REQUIRE(ostream.str() == out);
+}
+
+TEST_CASE("delition_when_brother_is_black_and_his_right_son_is_red")
+{
+    tree_t<int> tree{1,2,3,4,5,6,7,8,9,10,11,0};
+	
+	
+	tree.remove(3);
+	tree.remove(2);
+	
+	std::string out {
+		"--------11\n"
+		"------10\n"
+		"--------9\n"
+		"----8\n"
+		"------7\n"
+		"--6\n"
+		"------5\n"
+		"----4\n"
+		"------1\n"
+		"--------0\n"
+		
+	};
+
+	std::ostringstream ostream;
+	tree.print(ostream , tree.root(), 1);
+	
+	REQUIRE(ostream.str() == out);
+}
+
+TEST_CASE("delition_when_brother_is_black_and_his_sons")
+{
+    tree_t<int> tree{5 , 3 , 7 ,8 };
+	
+	tree.remove(8);
+	tree.remove(3);
+	
+	std::string out {
+		"----7\n"
+		"--5\n"
+	};
+
+	std::ostringstream ostream;
+	tree.print(ostream , tree.root(), 1);
+	
+	REQUIRE(ostream.str() == out);
+}
+
+TEST_CASE("delition_when_brother_is_red")
+{
+    tree_t<int> tree{5,3,7,8,6,2,1,9};
+	
+	
+	tree.remove(2);
+	
+	std::string out {
+		"--------9\n"
+		"------8\n"
+		"----7\n"
+		"------6\n"
+		"--5\n"
+		"----3\n"
+		"------1\n"
+	};
+
+	std::ostringstream ostream;
+	tree.print(ostream , tree.root(), 1);
+	
+	REQUIRE(ostream.str() == out);
+}
